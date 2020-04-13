822e52a7f731d6ec7cca02b30a3db071fce41989
rosa lee <rosalee0116@gmail.com>
2020-04-13 22:04:51 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,14 @@
 class Project
-  attr_accessor :backers
-  attr_reader :title
+
+  attr_reader :title, :backers 
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(backer)
-    backers << backer
-    backer.backed_projects << self
+  def add_backer(name)
+    @backers << name 
+    name.backed_projects << self
   end
 end
